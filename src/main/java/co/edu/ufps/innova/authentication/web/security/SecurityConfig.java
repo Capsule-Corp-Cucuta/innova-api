fed73f6f8cd5f70397573fa01c26c6f7ae01db21
Sergio Rodriguez Ramirez <sergioarodram@gmail.com>
2021-06-16 22:05:30 -0500

    restore security

--- src/main/java/co/edu/ufps/innova/authentication/web/security/SecurityConfig.java
+++ src/main/java/co/edu/ufps/innova/authentication/web/security/SecurityConfig.java
@@ -38,10 +38,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             // other public endpoints
-            //"/**/auth",
-            //"/**/contact",
-            //"/**/user/**/recover-password",
-            "/**"
+            "/**/auth",
+            "/**/contact",
+            "/**/user/**/recover-password",
     };
 
     @Bean
